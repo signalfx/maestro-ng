2afb8a14d0a461d5fd99ae5fbe02f0a84fe95f1c
Maxime Petazzoni <max@signalfuse.com>
2014-01-30 16:20:52 -0800

    Prepare for 0.1.7
    Signed-off-by: Maxime Petazzoni <max@signalfuse.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='maestro',
-    version='0.1.6',
+    version='0.1.7-dev',
     description='Orchestrator for multi-host Docker deployments',
     zip_safe=True,
     packages=find_packages(),

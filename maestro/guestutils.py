8c986805aa7214aae1bc5894cb49fffd4abca5e3
Maxime Petazzoni <max@signalfuse.com>
2014-02-20 13:41:20 -0800

    Throw more explicit exceptions in guestutils
    Signed-off-by: Maxime Petazzoni <max@signalfuse.com>

--- maestro/guestutils.py
+++ maestro/guestutils.py
@@ -64,28 +64,43 @@ def get_port(name, default=None):
 def get_specific_host(service, container):
     """Return the hostname/address of a specific container/instance of the
     given service."""
-    return os.environ['{}_{}_HOST'.format(_to_env_var_name(service),
-                                          _to_env_var_name(container))]
+    try:
+        return os.environ['{}_{}_HOST'.format(_to_env_var_name(service),
+                                              _to_env_var_name(container))]
+    except:
+        raise MaestroEnvironmentError(
+            'No host defined for container {} of service {}'
+            .format(container, service))
 
 
 def get_specific_exposed_port(service, container, port, default=None):
     """Return the exposed (internal) port number of a specific port of a
     specific container from a given service."""
-    return int(os.environ.get(
-        '{}_{}_{}_INTERNAL_PORT'.format(_to_env_var_name(service),
-                                        _to_env_var_name(container),
-                                        _to_env_var_name(port)).upper(),
-        default))
+    try:
+        return int(os.environ.get(
+            '{}_{}_{}_INTERNAL_PORT'.format(_to_env_var_name(service),
+                                            _to_env_var_name(container),
+                                            _to_env_var_name(port)).upper(),
+            default))
+    except:
+        raise MaestroEnvironmentError(
+            'No internal port {} defined for container {} of service {}'
+            .format(port, container, service))
 
 
 def get_specific_port(service, container, port, default=None):
     """Return the external port number of a specific port of a specific
     container from a given service."""
-    return int(os.environ.get(
-        '{}_{}_{}_PORT'.format(_to_env_var_name(service),
-                               _to_env_var_name(container),
-                               _to_env_var_name(port)).upper(),
-        default))
+    try:
+        return int(os.environ.get(
+            '{}_{}_{}_PORT'.format(_to_env_var_name(service),
+                                   _to_env_var_name(container),
+                                   _to_env_var_name(port)).upper(),
+            default))
+    except:
+        raise MaestroEnvironmentError(
+            'No port {} defined for container {} of service {}'
+            .format(port, container, service))
 
 
 def get_node_list(service, ports=[], minimum=1):

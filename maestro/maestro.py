86875f169b8f29113d39e6b7a99787b6fb77d23a
Maxime Petazzoni <max@signalfuse.com>
2014-06-11 10:29:47 -0700

    Fix maestro logs command. Fixes #46.
    Signed-off-by: Maxime Petazzoni <max@signalfuse.com>

--- maestro/maestro.py
+++ maestro/maestro.py
@@ -5,6 +5,7 @@
 from . import entities
 from . import exceptions
 from . import plays
+from . import termoutput
 
 
 class Conductor:
@@ -254,7 +255,7 @@ class Conductor:
 
         container = containers[0]
 
-        o = plays.OutputFormatter()
+        o = termoutput.OutputFormatter()
         o.pending('Inspecting container status...')
         status = container.status()
         if not status:

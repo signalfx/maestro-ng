b3746486342de4478e94c97693e07b4ece312325
soby <sobyx@hotmail.com>
2014-08-08 12:56:07 -0700

    Fixed missing reference to self

--- maestro/plays/tasks.py
+++ maestro/plays/tasks.py
@@ -191,7 +191,7 @@ class StartTask(Task):
             cpu_shares=self.container.cpu_shares,
             ports=ports,
             detach=True,
-            dns=container.dns,
+            dns=self.container.dns,
             command=self.container.command)
 
         self.o.pending('waiting for container creation...')

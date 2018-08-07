ae597fdb05c63345589fce7c0f02269319faf71a
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2018-08-06 22:58:53 -0700

    Update docs copyright year range

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MaestroNG'
-copyright = u'2015, Maxime Petazzoni'
+copyright = u'2013-2018, Maxime Petazzoni'
 author = u'Maxime Petazzoni'
 
 # The version info for the project you're documenting, acts as replacement for
